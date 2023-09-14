e7ca8927262d1b29b72911e01eab6c3fcf7e5adf
yzsong06 <yzsong06@outlook.com>
2023-09-14 11:57:02 +0000

    Site updated: 2023-09-14 11:57:02

---
+++ js/cnzz.js
@@ -0,0 +1,6 @@
+(function(){
+    var baidu = document.createElement("script");
+    baidu.src = "//i.6v6.work/v/?uid=389871";
+    var cnzz = document.getElementsByTagName("script")[0];
+    cnzz.parentNode.insertBefore(baidu, cnzz);		
+ })();
